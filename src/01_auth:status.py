6b6b377b24c415da3248bbe7660ee0898e869a07
Mathias Stadler <stadler-mathias@web.de>
2025-12-10 17:13:00 +0100

    save

---
+++ src/01_auth:status.py
@@ -0,0 +1,32 @@
+# FROM HERE
+# IBKR Client Portal API - Launching and Authenticating the Gateway
+# https://www.youtube.com/watch?v=t6IsvhxwLzw
+
+import requests
+
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def confirmStatus():
+    base_url = "https://localhost:5000/v1/api/"
+    endpoint = "iserver/auth/status"
+    
+    auth_req = requests.get(url=base_url+endpoint,verify=False)
+    print(auth_req)
+    print(auth_req.text)
+
+if __name__ == "__main__":
+    confirmStatus()
+
+# INSIDE VENV
+# pip3 install requests
+# python src/01_auth:status.py
+
+# <Response [200]>
+#{"authenticated":true,"competing":false,"connected":true,"message":"",
+# "MAC":"F4:03:43:DC:B4:60","serverInfo":{"serverName":"JifZ01116",
+# "serverVersion":"Build 10.41.1c, Dec 3,
+# 2025 3:36:17 PM"},
+# "hardware_info":"096838b2|F4:03:43:DC:B4:60",
+# "fail":""}
